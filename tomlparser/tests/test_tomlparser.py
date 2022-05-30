5a24a8c29b5afb5289bb34c4f3a79858c7000a98
fqjupe <jukka.pelli@tuni.fi>
2022-05-30 09:08:33 +0300

    updated tests

--- tomlparser/tests/test_tomlparser.py
+++ tomlparser/tests/test_tomlparser.py
@@ -5,12 +5,16 @@ Preliminary Assignment
 This module has some unittests for the tomlparser module
 """
 import unittest
-import tomlparser.tomlparser as parser
+import os
+from tomlparser.tomlparser import TomlParser
+
 
 class TestParser(unittest.TestCase):
     
     def test_known_project(self):
-        parsed = parser.TomlParser("testpoetry.lock")
+        test_filename = os.path.join(os.path.dirname(__file__), "testpoetry.lock")
+
+        parsed = TomlParser(test_filename)
 
         self.assertEqual(dict, type(parsed.get_data()))
 
@@ -20,7 +24,9 @@ class TestParser(unittest.TestCase):
 
 
     def test_empty_project(self):
-        parsed = parser.TomlParser("testempty.lock")
+        test_filename = os.path.join(os.path.dirname(__file__), "testempty.lock")
+        
+        parsed = TomlParser(test_filename)
 
         self.assertEqual(0, len(parsed.get_data()))
 
